5a9a4f62f4cc38ff9d39308fd98af89ab303efe4
smithacevedo <smithacevedo2002@gmail.com>
2023-10-15 14:51:37 -0500

    Se crea página de login

---
+++ Login.java
@@ -0,0 +1,40 @@
+import javax.swing.*;
+
+public class Login {
+    public static void main(String[] args) {
+        // Crear un nuevo marco (ventana) de la aplicación
+        JFrame frame = new JFrame("Login");
+        
+        // Crear etiquetas para usuario y contrasenia
+        JLabel usuarioLabel = new JLabel("Usuario:");
+        JLabel contraseniaLabel = new JLabel("Contraseña:");
+        
+        // Crear campos de texto para ingresar usuario y contrasenia
+        JTextField usuarioTextField = new JTextField(20);
+        // Utiliza JPasswordField para el campo de contraseña
+        JPasswordField contraseniaField = new JPasswordField(20);
+        
+        // Crear un botón para enviar el formulario
+        JButton enviarButton = new JButton("Enviar");
+        
+        // Crear un panel para organizar los componentes
+        JPanel panel = new JPanel();
+        
+        // Agregar los componentes al panel
+        panel.add(usuarioLabel);
+        panel.add(usuarioTextField);
+        panel.add(contraseniaLabel);
+        panel.add(contraseniaField);
+        panel.add(enviarButton);
+        
+        // Agregar el panel al marco
+        frame.add(panel);
+        
+        // Configurar el tamaño y la operación de cierre
+        frame.setSize(300, 150);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Hacer visible el marco
+        frame.setVisible(true);
+    }
+}
