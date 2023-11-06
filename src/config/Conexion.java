b864056b63e0b84d9fe254e27ea77997ed863638
santiago caicedo <santiago.vela.caicedo312@gmail.com>
2023-11-06 14:31:52 -0500

    ajustes de conexion

--- src/config/Conexion.java
+++ src/config/Conexion.java
@@ -6,26 +6,19 @@ import java.sql.SQLException;
 
 public class Conexion {
     Connection con;
-    Connection conEncuesta;
-    Connection conUsuarios;
-    Connection conAulas;
-    Connection conProfesores;
-    Connection conAdmin;
-    Connection conAmbientes;
 
-    
-    
-    public Conexion(){
+    public Conexion() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            con=DriverManager.getConnection("jdbc:mysql://localhost:3306/conexion", "root", "");
-        } catch (ClassNotFoundException | SQLException e){
-            System.err.println("No se pudo establecer conexion con la BD. Error: " + e);
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://ec2-18-205-44-21.compute-1.amazonaws.com:5432/d3873ku87gl6fs?currentSchema=PI2", "yeafisgfigvvac", "4e04fd077af6c99207fbfc9d616444298307a8b4c164a76d582bc27d910ea8cc");
+        } catch (ClassNotFoundException | SQLException e) {
+            System.err.println("No se pudo establecer conexión con la BD. Error: " + e);
         }
     }
 
-    
-    public Connection getConnection(){
+    public Connection getConnection() {
         return con;
-    }   
+    }
 }
+
+
